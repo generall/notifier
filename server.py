0886733368921b4c4054f4c5b5c47f157461f955
Andrei Vasnetsov <andrei.vasnetsov@moberries.com>
2020-10-02 10:53:55 +0200

    add timeout

--- server.py
+++ server.py
@@ -2,16 +2,29 @@ from flask import request
 from flask import Flask
 import requests
 import os
+from time import time 
+
 app = Flask(__name__)
 
 BOT_API = os.environ["BOT_API"]
 CHAT_ID = os.environ["CHAT_ID"]
+
+backoff = 30 # seconds
+last = 0
+
 
 @app.route('/', methods=['GET'])
 def notify():
     message = request.args.get('m')
     if not message:
         return "try ?m=your-message", 400
+
+    current_time = int(time())
+    if current_time - last < backoff:
+        return "Not so fast", 429
+
+    last = current_time
+
     requests.post(
         f"https://api.telegram.org/bot{BOT_API}/sendMessage",
         json={
